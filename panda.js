3382e81ca0651aace391a4dde7e9f482007f1332
unknown <mahmodulshahin665@gmail.com>
2022-03-04 22:08:58 +0600

    add more code

--- panda.js
+++ panda.js
@@ -1,1 +1,16 @@
-console.log('hey! I wanna to complete my dream with you. Can we ?')
+//for btn disableed
+document.getElementById('delete-confirm').addEventListener('keyup', function (event) {
+    const deleteBtn = document.getElementById('delete-btn');
+
+    if (event.target.value == 'email') {
+        deleteBtn.removeAttribute('disabled');
+    }
+    else {
+        deleteBtn.setAttribute('disabled', true)
+    }
+})
+// change
+document.getElementById('delete-confirm').addEventListener('change', function () {
+    const deleteField = document.getElementById('delete-confirm');
+    console.log(deleteField.value);
+})
